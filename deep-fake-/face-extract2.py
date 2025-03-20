568b95092e76beb0e3691370d1548df7768d87ca
negativenagesh <gaonkarsub@gmail.com>
2025-03-20 15:33:08 +0530

    parallel processing

--- deep-fake-/face-extract2.py
+++ deep-fake-/face-extract2.py
@@ -5,7 +5,7 @@ import cv2
 import face_recognition
 from PIL import Image, ImageDraw
 
-def extract_faces_from_images(input_folder, output_folder, start_index=38201, end_index=40293, use_gpu=True):
+def extract_faces_from_images(input_folder, output_folder, start_index=47000, end_index=50000, use_gpu=True):
     # Create output folder if it doesn't exist
     Path(output_folder).mkdir(parents=True, exist_ok=True)
     
@@ -76,4 +76,4 @@ def extract_faces_from_images(input_folder, output_folder, start_index=38201, en
 if __name__ == "__main__":
     input_folder = "/home/vu-lab03-pc24/Downloads/merged_shuffled_images"
     output_folder = "/home/vu-lab03-pc24/Downloads/deep-fake/extracted-faces-parallel-3"
-    extract_faces_from_images(input_folder, output_folder, start_index=38201, end_index=40293, use_gpu=True)
+    extract_faces_from_images(input_folder, output_folder, start_index=47000, end_index=50000, use_gpu=True)
