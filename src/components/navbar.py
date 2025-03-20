1806b43a7a87aeae10a5a202aaec3254de1098fc
negativenagesh <gaonkarsub@gmail.com>
2025-03-21 01:10:09 +0530

    small size, correct path etc

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -4,27 +4,27 @@ import os
 
 def render_navbar():
     # Load CSS
-    with open("landing_page/styles/styles.css") as f:
+    with open("src/styles/styles.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
-    logo_path = "landing_page/styles/images/resumai-high-resolution-logo.png"
+    logo_path = "UnFake-logo/logo.png"
     
     # Navbar HTML with anchor links for navigation
     navbar_html = f"""
     <div class="navbar">
         <div class="navbar-logo">
-            <img src="data:image/png;base64,{get_image_as_base64(logo_path)}" height="130">
+            <img src="data:image/png;base64,{get_image_as_base64(logo_path)}" height="150">
         </div>
         <div class="navbar-links">
-            <a href="#hero-section" style="font-size: 20px;">Home</a>
-            <a href="#why_resumai_exists" style="font-size: 20px;">About</a>
-            <a href="#powerful_features" style="font-size: 20px;">Features</a>
-            <a href="#how_resumai_works" style="font-size: 20px;">How It Works</a>
-            <a href="#resumai_benefits" style="font-size: 20px;">Benefits</a>
-            <a href="#success_stories" style="font-size: 20px;">Testimonials</a>
-            <a href="#faq" style="font-size: 20px;">FAQ</a>
-            <a href="" style="font-size: 20px;">Signup / Login </a>
-            <a href="https://github.com/negativenagesh/resumai" target="_blank">
+            <a href="#hero-section" style="font-size: 18px;">Home</a>
+            <a href="#why_resumai_exists" style="font-size: 18px;">About</a>
+            <a href="#powerful_features" style="font-size: 18px;">Technology</a>
+            <a href="#how_resumai_works" style="font-size: 18px;">Features</a>
+            <a href="#resumai_benefits" style="font-size: 18px;">Dataset</a>
+            <a href="#success_stories" style="font-size: 18px;">Demo</a>
+            <a href="#faq" style="font-size: 18px;">Use Cases</a>
+            <a href="" style="font-size: 18px;">Research</a>
+            <a href="https://github.com/negativenagesh/UnFake" target="_blank">
                 <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style="width: 30px; height: 30px; margin-left: 10px;">
             </a>
         </div>
