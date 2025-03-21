40dc354770462814d917fa5df1968e817f59750c
negativenagesh <gaonkarsub@gmail.com>
2025-03-21 23:08:03 +0530

    added pages

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 import sys
 import os
 
-# Make sure to add the parent directory to the path before importing from src
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
 from src.app import show_landing_page
+from src.components.image_scraper import render_image_search  # Import from new file
 
 st.set_page_config(
-    page_title="UnFake",  
-    page_icon="UnFake-logo/logo.png",  
+    page_title="UnFake",
+    page_icon="UnFake-logo/logo.png",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
@@ -23,13 +23,16 @@ def main():
         page_mapping = {
             "app": "main_app",
             "landing": "landing",
-            "auth": "auth"
+            "auth": "auth",
+            "image_scraper": "image_scraper"
         }
         if page_param in page_mapping:
             st.session_state.page = page_mapping[page_param]
 
     if st.session_state.page == "landing":
         show_landing_page()
+    elif st.session_state.page == "image_scraper":
+        render_image_search()
 
 if __name__ == "__main__":
     main()
