6c072c9009f3a686f10bf02b7beefc94df310566
negativenagesh <gaonkarsub@gmail.com>
2025-04-02 23:34:09 +0530

    added new button for UnFake API

--- src/components/hero_section.py
+++ src/components/hero_section.py
@@ -13,12 +13,6 @@ def render_hero_section():
         <p style="margin-top: 0; margin-bottom: 0;">images with unprecedented accuracy, helping safeguard digital media</p>
         <p style="margin-top: 0;">authenticity across selected categories of Unsplash images</p>
         <div class="hero-buttons">
-            <a href="#powerful_features" class="hero-button explore-btn"
-               style="margin-right: 20px; display: inline-block; text-align: center; 
-                      padding: 10px 24px; font-weight: bold; border-radius: 5px; 
-                      text-decoration: none; background-color: #ff4b4b; color: white;">
-               Try Demo
-            </a>
             <a href="?page=image_search" target="_self"
                class="hero-button how-it-works-btn"
                style="margin-right: 20px; display: inline-block; text-align: center; padding: 10px 24px; 
@@ -28,11 +22,18 @@ def render_hero_section():
             </a>
             <a href="?page=custom_image" target="_self"
                class="hero-button custom-image-btn"
-               style="display: inline-block; text-align: center; padding: 10px 24px; 
+               style="margin-right: 20px; display: inline-block; text-align: center; padding: 10px 24px; 
                       font-weight: bold; border-radius: 5px; text-decoration: none; 
                       background-color: #ff4b4b; color: white;">
                Test Custom Image
             </a>
+            <a href="?page=unfake_api" target="_self"
+               class="hero-button custom-image-btn"
+               style="display: inline-block; text-align: center; padding: 10px 24px; 
+                      font-weight: bold; border-radius: 5px; text-decoration: none; 
+                      background-color: #ff4b4b; color: white;">
+               UnFake API
+            </a>            
         </div>
     </div>
     """
